281313eecf1cf2568ea4fc8f62a57b240b9ea433
tiya adugna <tiyakefalee@gmail.com>
2020-04-03 03:24:48 +0000

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+ return "Hello #{player1} & #{player2}"
+ #binding.pry 
 end
 
-def play_game(player2)
+def play_game(player1,player2)
   "#{player1} is better than #{player2}"
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,16 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    num = 1  
+    str = "s"
+    while num < 10 do 
+    str = str + "s" 
+    num += 1 
+  end 
+  str = str + string
   else
-    string
+  string
   end
+  #binding.pry 
 end
